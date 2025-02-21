561de411571ff8013f36f650a452188eeb493bc7
lob23 <hvblong21@apcs.fitus.edu.vn>
2025-02-22 02:38:20 +0700

    Implementation of ema,rsi and vwap

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,37 @@
 import pandas as pd
 import numpy as np
 
+# Note: infput is the closes prices of each candle withifn the window. type is pandas series.
 def calculate_ema(data, period):
-    print("Not implemented")
-    
+    return pd.Series(data).ewm(span=period, adjust=False).mean()
+
+
 def calculate_rsi(data, period=14):
-    print("Not calculated")
+    delta = pd.Series(data).diff()
     
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+    
+    avg_gain = pd.Series(gain).ewm(span=period, adjust=False).mean()
+    avg_loss = pd.Series(loss).ewm(span=period, adjust=False).mean()
+    
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    rsi = np.where(avg_loss == 0, 100, rsi)
+    return pd.Series(rsi, index=data.index)
+    
+
 def calculate_vwap(data):
-    print("Not calculated")
+    data = data.dropna(subset=['High', 'Low', 'Close', 'Volume'])
+    data = data[data['Volume'] > 0]
+    
+    typical_price = (data['High'] + data['Low'] + data['Close'])/3
+
+
+    cumulative_vol = np.cumsum(data['Volume'])
+    cumulative_vwap = np.cumsum(typical_price * data['Volume'])/cumulative_vol
+    
+    return pd.Series(cumulative_vwap, index=data.index)
     
 def calculate_atr(data, period=14):
     print("Not calculated")
