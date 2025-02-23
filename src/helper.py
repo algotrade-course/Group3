4d8d101801a01d9f8442b85e3fe52751301eda77
lob23 <hvblong21@apcs.fitus.edu.vn>
2025-02-23 21:17:53 +0700

    update close and open position criteria

--- src/helper.py
+++ src/helper.py
@@ -1,5 +1,5 @@
 from typing import List, Tuple
-
+from utils import calculate_ema, calculate_rsi, calculate_vwap, calculate_atr, detect_trend
 
 def open_position(
     position_type: str,
@@ -52,5 +52,44 @@ def close_positions(
     
     return new_holdings, total_realized_pnl, total_unrealized_pnl
 
-def position_type():
-    raise ValueError("Not implemented")
+#Output: 
+# 0: No action
+# 1: open long position
+# 2: open short position
+
+def open_position_type(data, stock, cur_price):
+    ema5 = calculate_ema(data['Close'], 5)
+    ema20 = calculate_ema(data['Close'],20)
+    vwap = calculate_vwap(data)
+    rsi = calculate_rsi(data, 14)
+    
+    if (detect_trend[0] == True or rsi<30 or rsi>70):
+        return 0
+    
+    long_criteria = int(ema5>ema20) + int(cur_price > vwap) + int(rsi >= 50) + int(rsi < 70) + int(detect_trend[1] == True)
+    short_criteria =  int(ema5<ema20) + int(cur_price < vwap) + int(rsi<50) + int(rsi>30) + int(detect_trend[1]==False)
+    
+    if (long_criteria > 3):
+        return 1
+    if (short_criteria > 3):
+        return 2
+
+# Output:
+## 0: Noo action
+## 1: Close Long position
+## 2: Close short position
+## Note: Check if the holding is close before acting close long position, and otherwise.
+def close_position_type(data, stock, cur_price):
+    ema5 = calculate_ema(data['Close'], 5)
+    ema20 = calculate_ema(data['Close'],20)
+    rsi = calculate_rsi(data, 14)
+
+    close_long_criteria = int(ema5 < ema20) + int(rsi>70) + int(cur_price>ema20)
+    close_short_criteria = int(ema5 > ema20) + int(rsi<30) + int(cur_price<ema5)
+    
+    if (close_long_criteria >= 1):
+        return 1
+    elif (close_short_criteria >= 1):
+        return 2
+    else:
+        return 0
---
+++ src/test.py
@@ -0,0 +1,3 @@
+num = int(6>2) + int(4>3) + int(True)
+
+print(num)
